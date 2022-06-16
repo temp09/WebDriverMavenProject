a7d4e0cb84dcacab61654d545ad633937c8036e5
Mohammed Ullah <amdad71@yahoo.com>
2022-06-16 17:10:50 -0500

    updated src/

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,35 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+
+		driver = new FirefoxDriver();
+
+	}
+
+	@Test
+	public void doLogin() {
+
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.id("Pawd")).sendKeys("sdfdsf");
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+	}
+
+}
